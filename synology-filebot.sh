a93425eab1de047097bb734f302d26727cf52079
Jan Zavrl <jan.zavrl@agiledrop.com>
2016-04-03 22:01:00 +0200

    Init and move script

--- synology-filebot.sh
+++ synology-filebot.sh
@@ -12,15 +12,39 @@ USAGE
 exit 0
 }
 
-# Scan the folders to find new files
-sf-scan() {
+# Define global variables
+input="/Users/JanZavrl/Development/Temp/Downloads"
+output="/Users/JanZavrl/Development/Temp/Videos"
 
+# Scan the folders to find new files
+sf-init() {
+	# Loop over our input folder to get the first level folders
+	for i in "$input"/*
+	do
+		# Call our rename and move script with the path
+		sf-move $i
+	done
 }
 
 # Moves the movie or TV show and renames it properly
 sf-move() {
+	path=$1
+	folder=$(basename $path)
 
-	exit 0
+	echo "$input/$folder/*"
+
+	# Check if we have a series folder
+	if [ $folder == "Series" ]
+	then
+		# Filebot command for series
+		filebot -rename $input/$folder/* --format "$output/$folder/{n}/Season {s}/{n} - {s00e00} - {t}" --db TheTVDB --action copy
+	else
+		# Filebot command for movies
+		filebot -rename $input/$folder/* --format "$output/$folder/{n}/{n} - ({y})" --db TheMovieDB --action copy
+	fi
+
+	# Delete all files that are below 100M so we remain with only video files
+	find $output/$folder -type f ! -size +100M -delete
 }
 
 # Downloads the subtitles for the movie or TV show
@@ -34,3 +58,13 @@ sf-cleanup() {
 
 	exit 0
 }
+
+# Loop to read options and arguments.
+while [ $1 ]; do
+	case "$1" in
+		'-s')
+			sf-init
+			;;
+	esac
+	shift
+done
