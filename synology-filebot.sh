54bfd62bd7ebbba8bdccc0060ba35f143f06e989
Jan Zavrl <jan.zavrl@agiledrop.com>
2016-04-03 19:57:12 +0200

    Initial commit

---
+++ synology-filebot.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# This script renames a movie or TV show using filebot,
+# moves it to the desired location inside a NAS
+# and downloads subtitles.
+
+# Show help information about the script
+sf-help() {
+cat <<"USAGE"
+Write help text
+USAGE
+exit 0
+}
+
+# Scan the folders to find new files
+sf-scan() {
+
+}
+
+# Moves the movie or TV show and renames it properly
+sf-move() {
+
+	exit 0
+}
+
+# Downloads the subtitles for the movie or TV show
+sf-subtitles() {
+
+	exit 0
+}
+
+# Deletes the old remaining files and folders
+sf-cleanup() {
+
+	exit 0
+}
